3dd280d68b023eb7b7edace4684d3605ce3687da
Pavlonto <pavelpiakarski@yandex.by>
2022-05-22 11:57:45 +0300

    addTemplateStrings

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -10,9 +10,12 @@ type CounterType = {
 }
 
 export const Counter = (props: CounterType) => {
+
+    const className = `${s.value} ${props.value > 4 ? s.valueFive : ''}`
+
     return (
         <div className={s.counter}>
-            <div className={props.value > 4 ? s.valueFive : s.value}>{props.value}</div>
+            <div className={className}>{props.value}</div>
             <div className={s.buttons}>
                 <Button disabled={props.value > 4} onClick={props.start} name={props.value > 0 ? "Add" : "Start"}/>
                 <Button disabled={props.value === 0} onClick={() => props.setValue(0)} name={"Reset"}/>
@@ -20,3 +23,5 @@ export const Counter = (props: CounterType) => {
         </div>
     )
 }
+
+// `${s.error} ${spanClassName ? spanClassName : ''}`
